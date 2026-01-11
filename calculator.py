6c8f36b4e78cc7ae82e2d70eb0cd78df55052079
Koba Hiro <h4330280@gmail.com>
2026-01-11 21:48:41 +0900

    電卓アプリif文・入出力

---
+++ calculator.py
@@ -0,0 +1,22 @@
+num1 = int(input("1つ目の数値を入力してください: "))
+
+sign = input("演算子を入力してください (+, -, *, /): ")
+
+num2 = int(input("2つ目の数値を入力してください: "))
+
+
+
+
+
+# def calculator(num1,num2):
+if num1 + num2:
+    print( num1 + num2 )
+elif num1 - num2 :
+    print( num1 - num2 )
+elif num1 * num2 :
+    print( num1 * num2 )
+elif  num1 / num2  :
+    print( num1 / num2 ) 
+else: print("計算を続けますか？(y/n)") 
+    
+      
