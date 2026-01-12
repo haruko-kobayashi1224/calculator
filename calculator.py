15d862b9bce97b3e1a2be62045657254c9e14cb2
Koba Hiro <h4330280@gmail.com>
2026-01-12 21:05:23 +0900

    例外処理・演算子実装

--- calculator.py
+++ calculator.py
@@ -1,8 +1,10 @@
 answer=""
-
    
 while answer != "n":
-    
+  
+  class SignInvalidError(Exception):
+    pass
+  
   try:    
 
     num1 = float(input("1つ目の数値を入力してください: "))
@@ -11,7 +13,9 @@ while answer != "n":
 
     num2 = float(input("2つ目の数値を入力してください: "))
     
-        
+   
+    if sign != "+" and sign != "-" and sign != "*" and sign != "/":
+      raise SignInvalidError("演算子を入力してください")    
     if sign == "+":
       print("結果:" + str(num1) + "+" + str(num2) + "=" + str(num1+num2))
     elif sign == "-":
@@ -20,13 +24,19 @@ while answer != "n":
       print("結果:" + str(num1) + "*" + str(num2) + "=" + str(num1*num2))
     elif  sign == "/": 
       print("結果:" + str(num1) + "/" + str(num2) + "=" + str(num1/num2))
-      
+    
     answer = input("計算を続けますか？(y/n)") 
     
   except ZeroDivisionError:
     print("0で割ることはできません")  
-    answer = input("計算を続けますか？(y/n)")   
-    
+    answer = input("計算を続けますか？(y/n)") 
+  except ValueError:
+    print("数字を入力してください")    
+    answer = input("計算を続けますか？(y/n)") 
+  except SignInvalidError as e:
+    print(e)
+    answer = input("計算を続けますか？(y/n)")      
+           
             
 if answer == "n":
   print("プログラムを終了します。")
