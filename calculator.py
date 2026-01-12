2db3303cdbeeae9ca12a170a6c826adfd8a01d6b
Koba Hiro <h4330280@gmail.com>
2026-01-12 21:31:28 +0900

    finally実装

--- calculator.py
+++ calculator.py
@@ -1,4 +1,4 @@
-answer=""
+answer = ""
    
 while answer != "n":
   
@@ -24,19 +24,15 @@ while answer != "n":
       print("結果:" + str(num1) + "/" + str(num2) + "=" + str(num1/num2))
     else: raise SignInvalidError("演算子を入力してください") 
     
-    answer = input("計算を続けますか？(y/n)") 
-    
   except ZeroDivisionError:
     print("0で割ることはできません")  
-    answer = input("計算を続けますか？(y/n)") 
   except ValueError:
     print("数字を入力してください")    
-    answer = input("計算を続けますか？(y/n)") 
   except SignInvalidError as e:
     print(e)
+  finally:  
     answer = input("計算を続けますか？(y/n)")      
-           
-            
+                       
 if answer == "n":
   print("プログラムを終了します。")
   
